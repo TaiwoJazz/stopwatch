9536c7a2ea6783c398cafdfbc4ae8fe3b6e8f7a0
TaiwoJazz <babsman4all@gmail.com>
2022-08-24 02:14:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+const btns = document.querySelectorAll(".btn");
+const items = document.querySelectorAll('.timer h2')
+
+let minutes = 0;
+let seconds = 0;
+let miliSeconds = 0;
+
+btns.forEach(function (e) {
+  e.addEventListener("click", function () {
+    if (e.dataset.id == 1) {
+      stopMiliseconds = setInterval(countMiliseconds, 10);
+    } else if (e.dataset.id == 2) {
+      clearInterval(stopMiliseconds);
+    } else {
+      clearInterval(stopMiliseconds);
+      resetValue();
+    }
+  });
+});
+
+const countMiliseconds = () => {
+  miliSeconds++;
+  if (miliSeconds == 100) {
+    miliSeconds = 0;
+    seconds++;
+  }
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+  }
+  displayStopWatch();
+};
+
+const resetValue = () => {
+  minutes = 0;
+  seconds = 0;
+  miliSeconds = 0;
+  items.textContent = displayStopWatch();
+};
+
+const displayStopWatch = () => {
+  const values = [minutes, seconds, miliSeconds]
+  function format(item) {
+    if (item < 10) {
+      return item = `0${item}`
+    }
+    return item
+  };
+  
+  items.forEach(function (item, index) {
+    item.textContent = format(values[index]);
+  });
+}
+displayStopWatch();
