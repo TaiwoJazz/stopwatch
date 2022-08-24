86057f64d6696a3a1fcb5f8280cdc466b68ce74a
TaiwoJazz <babsman4all@gmail.com>
2022-08-24 02:38:42 +0100

    error correction

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ btns.forEach(function (e) {
       stopMiliseconds = setInterval(countMiliseconds, 10);
     } else if (e.dataset.id == 2) {
       clearInterval(stopMiliseconds);
+    } else {
       clearInterval(stopMiliseconds);
       resetValue();
     }
